20c61c449e0855891672bb85c15f16047225f0b8
lava-develpers <83-rinat_bagauv@users.noreply.gitlab.example.com>
2024-02-07 11:01:49 +0300

    fix

--- src/Http/Client/HttpClient.php
+++ src/Http/Client/HttpClient.php
@@ -7,29 +7,27 @@ use Lava\Api\Constants\ClientConstants;
 use Lava\Api\Contracts\Client\HttpClientContract;
 use Lava\Api\Exceptions\BaseException;
 
-class HttpClient implements HttpClientContract
-{
+class HttpClient implements HttpClientContract {
 
     /**
      * @param string $method
      * @param string $data
      * @param int $timeout
+     *
      * @return array
      * @throws BaseException
      * @throws JsonException
      */
-    public function postRequest(string $method, string $data, int $timeout = 5): array
-    {
+    public function postRequest(string $method, string $data, int $timeout = 5): array {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, ClientConstants::URL . $method);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Accept: application/json',
-            'Content-Type: application/json'
-        ]);
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER, ['Accept: application/json', 'Content-Type: application/json']
+        );
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         $result = curl_exec($ch);
@@ -46,21 +44,20 @@ class HttpClient implements HttpClientContract
      * @param string $method
      * @param array $data
      * @param int $timeout
+     *
      * @return array
      * @throws BaseException
      * @throws JsonException
      */
-    public function getRequest(string $method, array $data, int $timeout = 5): array
-    {
+    public function getRequest(string $method, array $data, int $timeout = 5): array {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, ClientConstants::URL . $method . '/' . http_build_query($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Accept: application/json',
-            'Content-Type: application/json'
-        ]);
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER, ['Accept: application/json', 'Content-Type: application/json']
+        );
         curl_setopt($ch, CURLOPT_POST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         $result = curl_exec($ch);
